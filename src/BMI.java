38d3b170ba5e55ffa4bc9dd15907d423a33b6ef3
Dr. Dillinger <peter.dillinger@srh.de>
2023-01-18 15:09:17 +0100

    Initial commit

---
+++ src/BMI.java
@@ -0,0 +1,16 @@
+public class BMI {
+    // Aufgabenstellung:
+    // Eingabe:
+    // - Name
+    // - Geburtsjahr
+    // - Gewicht
+    // - Größe
+    // Ausgabe (vielleicht ein bisschen netter formuliert):
+    // - Alter
+    // - BMI (Gewicht [kg] / Größe² [m])
+    // - Einschätzung des BMI (Version 2)
+
+    public static void main(String[] args) {
+
+    }
+}
